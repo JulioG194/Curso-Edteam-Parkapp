de9fcfcde1e85ff3153a90702863519bb6a086c0
Julio <julio.gonzalez2014a@gmail.com>
2019-12-30 09:05:52 -0500

    Proyecto subido - notificaciones Firebase-firestore

---
+++ public/js/config/ConfigFirebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyABWvVQyg2IMHZZjprs_HFVxgZUyKK3Z9k",
+    authDomain: "parqueaderos-edteam.firebaseapp.com",
+    databaseURL: "https://parqueaderos-edteam.firebaseio.com",
+    projectId: "parqueaderos-edteam",
+    storageBucket: "parqueaderos-edteam.appspot.com",
+    messagingSenderId: "517212857951",
+    appId: "1:517212857951:web:f951776aac0f80c7da9dea",
+    measurementId: "G-P9336DW426"
+};
